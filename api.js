fa1dc4d7acfb6d5495e06420344c0ef0f17ea101
Your Name <laura.kotlinska.96@gmail.com>
2025-12-05 13:19:36 +0100

    Dynamic wine and recipe pairings

--- api.js
+++ api.js
@@ -1,56 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-  // QUESTION: WILL THIS COUNT AS HAVING MULTIPLE APIs?
-
+// QUESTION: WILL THIS COUNT AS HAVING MULTIPLE APIs?
+export function getPairedRecipes(wineType) {
   const foodAPIBaseUrl = `https://api.spoonacular.com/recipes/`;
   const wineAPIBaseUrl = "https://api.spoonacular.com/food/wine/";
-  
 
-  const wineType = 'merlot';
-
-
-  // console.log(wineType);
   const pairingSearch = new URLSearchParams({
     wine: wineType,
     apiKey: "bdf9eb43726a4fec87cc0d370296753d",
   });
 
-  
   const getPairings = async () => {
-      
-    // ---- Get dish examples from wine API
+    // Get dish examples from wine API
     const response = await fetch(wineAPIBaseUrl + "dishes?" + pairingSearch);
     const pairingData = await response.json();
-    console.log(pairingData);
+    // Create array with pairings
     const pairings = pairingData["pairings"];
-    const pairingTags = pairings.join(",");
-    console.log(pairings.length());
-    
-    const pairingExample = [ "tacos", "curry", "sushi" ]
-    
-    const getRecipe = async (ingredient) => {
 
+    const getRecipe = async (query) => {
       const dishSearch = new URLSearchParams({
-        'query': ingredient,
-        'number': 1,
-        'apiKey' : "bdf9eb43726a4fec87cc0d370296753d",
+        query: query,
+        number: 1,
+        apiKey: "bdf9eb43726a4fec87cc0d370296753d",
       });
-
+      
       const response = await fetch(foodAPIBaseUrl + "complexSearch?" + dishSearch);
       const dishData = await response.json();
-      console.log(dishData);
-
+      return dishData;
     };
     getRecipe();
 
+    // Loops through pairings array to get a recipe per ingredient
     const getDishesByIngredient = async () => {
-      for (const ingredient of pairingExample) {
-        await getRecipe(ingredient);
+      for (const query of pairings) {
+        await getRecipe(query);
       }
     };
     getDishesByIngredient();
   };
   getPairings();
 
-
-});
+}
