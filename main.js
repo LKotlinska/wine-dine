7214f1c378b36d2d181f37be4e536dcc6bb721ff
Your Name <laura.kotlinska.96@gmail.com>
2025-12-05 13:20:09 +0100

    Importing the API file to main.js

--- main.js
+++ main.js
@@ -1,34 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
+import { getPairedRecipes } from "./api.js";
 
-    const dropdown = document.getElementById('wine-select');
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdown = document.getElementById("wine-select");
+  const button = document.getElementById("submit-winetype");
 
-    try{
-        fetch('resources/wines.json')
-        .then (response => response.json())
-        .then((wines) => {
+  button.addEventListener("click", () => {
+    // On click: select and submit value
+    const dropdown = document.getElementById("wine-select");
+    const wineType = dropdown.value;
+    // Reset to default
+    dropdown.value = "default";
+    
+    getPairedRecipes(wineType);
+  });
+  
+  // Fetch and display wine selection
+  try {
+    fetch("resources/wines.json")
+      .then((response) => response.json())
+      .then((wines) => {
+        for (let category in wines) {
+          // Name cleanup and create selection groups
+          const categoryName = replaceUnderscore(category);
+          const optgroup = document.createElement("optgroup");
+          optgroup.setAttribute("label", categoryName);
+          dropdown.appendChild(optgroup);
 
-            for (let category in wines) {
+          wines[category].forEach((wine) => {
+            const wineName = replaceUnderscore(wine);
+            const option = document.createElement("option");
+            option.innerText = wineName;
+            option.setAttribute("value", wine);
+            optgroup.appendChild(option);
+          });
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
 
-                // Name cleanup and create selection groups
-                const categoryName = replaceUnderscore(category);
-                const optgroup = document.createElement('optgroup');
-                optgroup.setAttribute('label', categoryName)
-                dropdown.appendChild(optgroup);
-                
-                wines[category].forEach(wine => {
-                    const wineName = replaceUnderscore(wine);
-                    const option = document.createElement('option');
-                    option.innerText = wineName;
-                    option.setAttribute('value', wine)
-                    optgroup.appendChild(option);
-                });
-            }
-        })
-    } catch (error) {
-        console.log(error);
-    }
 
-    function replaceUnderscore(string) {
-        return string.replace('_', ' ')
-    }
-})
+
+
+  function replaceUnderscore(string) {
+    return string.replace("_", " ");
+  }
+});
