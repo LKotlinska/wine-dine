7e21678280f0b03d18233f57b48bb57ff4958310
Your Name <laura.kotlinska.96@gmail.com>
2025-12-08 16:45:44 +0100

    Async bugfix of returning undefined data and code cleanup

--- api.js
+++ api.js
@@ -1,25 +1,24 @@
 // QUESTION: WILL THIS COUNT AS HAVING MULTIPLE APIs?
 export async function getPairedRecipes(wineType) {
   const foodAPIBaseUrl = `https://api.spoonacular.com/recipes/`;
-  const wineAPIBaseUrl = "https://api.spoonacular.com/food/wine/";
+  const wineAPIBaseUrl = 'https://api.spoonacular.com/food/wine/';
+  const apiKey = 'bdf9eb43726a4fec87cc0d370296753d';
 
-  const pairingSearch = new URLSearchParams({
-    wine: wineType,
-    apiKey: "bdf9eb43726a4fec87cc0d370296753d",
-  });
-
-  const getPairings = async () => {
+  const getPairings = async (wine) => {
+    const pairingSearch = new URLSearchParams({
+      wine,
+      apiKey
+    });
     // Get dish pairings from wine API
     const response = await fetch(wineAPIBaseUrl + "dishes?" + pairingSearch);
     const pairingData = await response.json();
-    // console.log(pairingData);
     return pairingData;
   };
   const getRecipe = async (query) => {
     const dishSearch = new URLSearchParams({
       query,
       number: 1,
-      apiKey: "bdf9eb43726a4fec87cc0d370296753d",
+      apiKey
     });
 
     const response = await fetch(
@@ -29,24 +28,21 @@ export async function getPairedRecipes(wineType) {
     return dishData;
   };
 
-  const pairings = await getPairings();
+  const pairingData = await getPairings(wineType);
   const recipes = [];
 
-  recipes.push(pairings["text"]);
-
-  for (const query of pairings["pairings"]) {
+  for (const query of pairingData.pairings) {
     // Set timeout cuz stoopid max 5 request/s
-    setTimeout(async () => {
-      // Extracting results[0] out of the array which contains recipe info
-      const recipeData = await getRecipe(query);
-      const newRecipe = recipeData?.results?.[0];
+    await new Promise(res => setTimeout(res, 250));
+    // Extracting results[0] out of the array which contains recipe info
+    const recipeData = await getRecipe(query);
+    const newRecipe = recipeData.results[0];
 
-      // Making sure the data is not undefined
-      if (newRecipe !== undefined) {
-        recipes.push(newRecipe);
-      } 
-    }, 250);
+    // Making sure the data is not undefined
+    if (newRecipe !== undefined) {
+      recipes.push(newRecipe);
+    }
   }
 
-  return recipes;
+  return { wineInfo: pairingData.text, recipes };
 }
