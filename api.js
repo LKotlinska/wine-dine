5f04ceec135c4f9b2ca1a93338741309832e8b9c
Your Name <laura.kotlinska.96@gmail.com>
2025-12-08 16:50:20 +0100

    Comments

--- api.js
+++ api.js
@@ -1,19 +1,21 @@
-// QUESTION: WILL THIS COUNT AS HAVING MULTIPLE APIs?
 export async function getPairedRecipes(wineType) {
   const foodAPIBaseUrl = `https://api.spoonacular.com/recipes/`;
   const wineAPIBaseUrl = 'https://api.spoonacular.com/food/wine/';
   const apiKey = 'bdf9eb43726a4fec87cc0d370296753d';
 
+  // ---- GET DISH PAIRINGS FROM WINE API
   const getPairings = async (wine) => {
     const pairingSearch = new URLSearchParams({
       wine,
       apiKey
     });
-    // Get dish pairings from wine API
+
     const response = await fetch(wineAPIBaseUrl + "dishes?" + pairingSearch);
     const pairingData = await response.json();
     return pairingData;
   };
+
+  // ---- GET DISH INFO FROM FOOR API
   const getRecipe = async (query) => {
     const dishSearch = new URLSearchParams({
       query,
@@ -28,12 +30,14 @@ export async function getPairedRecipes(wineType) {
     return dishData;
   };
 
+  // ---- GET RECIPE EXAMPLES FOR EACH PAIRING
   const pairingData = await getPairings(wineType);
   const recipes = [];
 
   for (const query of pairingData.pairings) {
     // Set timeout cuz stoopid max 5 request/s
     await new Promise(res => setTimeout(res, 250));
+
     // Extracting results[0] out of the array which contains recipe info
     const recipeData = await getRecipe(query);
     const newRecipe = recipeData.results[0];
